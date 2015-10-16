19c7b3fd3b07f68eb725adec01eb82372a3ebbc8
Andrew Brennwald <leopawnz@gmail.com>
2015-10-16 14:23:13 -0700

    Implement post editing.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,21 @@ class PostsController < ApplicationController
     render :show
   end
 
+  def edit
+    @post = Post.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "Post updated"
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :content)
--- spec/features/post_pages_spec.rb
+++ spec/features/post_pages_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'add a new post process' do
   it 'adds a new post' do
     visit posts_path
-    click_on 'create new'
+    click_on 'Create New'
     fill_in 'Title', :with => 'test post'
     fill_in 'Content', :with => 'test content'
     click_on 'Create Post'
@@ -12,8 +12,29 @@ describe 'add a new post process' do
 
   it 'gives errors if required info is missing' do
     visit posts_path
-    click_on 'create new'
+    click_on 'Create New'
     click_on 'Create Post'
+    expect(page).to have_content 'Please fix'
+  end
+end
+
+describe 'edit a post process' do
+  it 'edits a post' do
+    test_post = Post.create(title: "First post", content: "Just a test post for my new blog.")
+    visit post_path(test_post)
+    click_on 'Edit Post'
+    fill_in 'Title', :with => 'Edited post'
+    fill_in 'Content', :with => 'Edited content'
+    expect(page).not_to have_content 'First post'
+  end
+
+  it 'gives errors if required info is missing' do
+    test_post = Post.create(title: "First post", content: "Just a test post for my new blog.")
+    visit post_path(test_post)
+    click_on 'Edit Post'
+    fill_in 'Title', :with => ''
+    fill_in 'Content', :with => ''
+    click_on 'Update Post'
     expect(page).to have_content 'Please fix'
   end
 end
